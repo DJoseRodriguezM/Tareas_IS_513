272793dead61e75105a63316d6b6870a90363e65
David Jose Rodriguez <dj.rodriguez@unah.hn>
2024-02-24 00:00:36 -0600

    project

--- tarea_pantalla_20222000953/lib/main.dart
+++ tarea_pantalla_20222000953/lib/main.dart
@@ -18,46 +18,38 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             leading: IconButton(
               icon: const Icon(Icons.close),
-              onPressed: () {
-                // Lógica para cerrar la aplicación
-              },
+              onPressed: () {},
             ),
             title: const Text(
               'Cuentas de Google',
               style: TextStyle(
-                fontSize: 15, // Adjust the font size as needed
-                fontWeight: FontWeight.bold, // Add font weight if desired
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
               ),
             ),
             actions: [
               IconButton(
                 icon: const Icon(Icons.info),
-                onPressed: () {
-                  // Lógica para mostrar información
-                },
+                onPressed: () {},
                 iconSize: 20,
               ),
               IconButton(
                 icon: const Icon(Icons.search),
-                onPressed: () {
-                  // Lógica para realizar la búsqueda
-                },
+                onPressed: () {},
                 iconSize: 20,
               ),
               IconButton(
                 icon: Image.network(
                     'https://cdn-icons-png.flaticon.com/128/32/32904.png'),
-                onPressed: () {
-                  // Lógica para realizar la búsqueda
-                },
+                onPressed: () {},
                 iconSize: 20,
               )
             ],
             backgroundColor: Colors.grey[200],
             bottom: const TabBar(
               isScrollable: true,
-              indicatorColor: Colors.blue, // Set the indicator color to blue
-              labelColor: Colors.blue, // Set the label color to blue
+              indicatorColor: Colors.blue,
+              labelColor: Colors.blue,
               tabs: [
                 Tab(
                   text: 'Página Principal',
@@ -73,21 +65,42 @@ class MyApp extends StatelessWidget {
           ),
           body: const TabBarView(
             children: [
-              
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        'Tu cuenta está protegida',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
+              Card(
+                margin: EdgeInsets.all(0.0),
+                color: Colors.white,
+                child: ListTile(
+                  tileColor: Colors.white,
+                  selectedTileColor: Colors.white,
+                  dense: false,
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 35.0, horizontal: 15.0),
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                        color: Color.fromARGB(255, 218, 218, 218), width: 1.0),
+                    borderRadius: BorderRadius.zero,
                   ),
-                ],
-              )
+                  title: Text(
+                    'Tu cuenta está protegida ',
+                    style: TextStyle(color: Colors.black),
+                    textAlign: TextAlign.left,
+                  ),
+                  subtitle: Column(children: [
+                    Text(
+                      'La Verificacion de seguridad revisó tu cuenta y no encontró acciones recomendadas.',
+                      style: TextStyle(color: Colors.grey),
+                      textAlign: TextAlign.left,
+                    ),
+                    Text(
+                      'Más información',
+                      style: TextStyle(color: Colors.blue),
+                      textAlign: TextAlign.left,
+                    ),
+                  ]),
+                  trailing: FlutterLogo(
+                    size: 100,
+                  ),
+                ),
+              ),
             ],
           ),
         ),
--- tarea_pantalla_20222000953/lib/src/modules/principal/pages/home_page.dart
+++
@@ -1,2 +0,0 @@
-// import 'package:flutter/material.dart';
-// import 'package:clone_whatsapp/src/shared/widgets/custom_icon_button.dart';
